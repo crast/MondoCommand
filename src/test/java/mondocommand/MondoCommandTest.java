a2e161df427d1c4607af3e7a9493dfee3ecd850a
crast <contact@jamescrasta.com>
2013-02-18 14:37:21 -0700

    Add a test, and testing push to see if jenkins does its thing

--- src/test/java/mondocommand/MondoCommandTest.java
+++ src/test/java/mondocommand/MondoCommandTest.java
@@ -1,5 +1,17 @@
 package mondocommand;
 
-class MondoCommandTest {
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MondoCommandTest {
+    public MondoCommandTest() {
+        
+    }
+
+    @Test
+    public void testInitialize() {
+        MondoCommand c = new MondoCommand();
+        assertEquals("foo", c.addSub("foo").getName());
+    }
 }
