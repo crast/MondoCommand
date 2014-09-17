e10553b63310ed82ccc96ede7b4883b621f6b242
Saladoc <saladoc@heridan.de>
2014-09-17 02:13:21 +0200

    Add another Test for working dynamically registered Subcommands, just to be safe

--- src/test/java/mondocommand/dynamic/DynamicSubcommandsTest.java
+++ src/test/java/mondocommand/dynamic/DynamicSubcommandsTest.java
@@ -13,22 +13,36 @@ public class DynamicSubcommandsTest {
 
     @Test
     public void testMondoFailureWithDynamicSubcommands() {
-        MondoCommand cmd = new FailingCommand();
+        MondoCommand cmd = new DynamicCommand();
         MockCommandSender sender = new MockCommandSender();
         cmd.onCommand(sender, null, "test", new String[] { "fail" });
         assertEquals(1, sender.messages.size());
         assertEquals(sender.messages.get(0), ChatColor.RED + "fail");
     }
     
-    public static class FailingCommand extends MondoCommand {
+    @Test
+    public void testWorkingDynamicSubcommand() {
+        MondoCommand cmd = new DynamicCommand();
+        MockCommandSender sender = new MockCommandSender();
+        cmd.onCommand(sender, null, "test", new String[] {"success"});
+        assertEquals(1, sender.messages.size());
+        assertEquals(sender.messages.get(0), ChatColor.GREEN + "success");
+    }
+    
+    public static class DynamicCommand extends MondoCommand {
         
-        public FailingCommand() {
+        public DynamicCommand() {
             this.autoRegisterFrom(this);
         }
         
         @Sub
         public void fail(CallInfo call) throws MondoFailure {
             throw new MondoFailure("fail");
+        }
+        
+        @Sub
+        public void success(CallInfo call) {
+            call.reply("{GREEN}success");
         }
     }
 
